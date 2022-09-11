1ca51d99ea98746d0aab17d5db7f69e2e7d5ec75
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2022-09-11 10:13:00 +0530

    Create pics.py

---
+++ plugins/pics.py
@@ -0,0 +1,23 @@
+PICS = (
+            "https://telegra.ph/file/ca500c0bd49e7d6cca103.jpg",
+            "https://telegra.ph/file/65aabd2dc3277bb20cfbe.jpg",
+            "https://telegra.ph/file/b200909209c3d64f6d4b9.jpg",
+            "https://telegra.ph/file/e73931a581167d0ae1211.jpg",
+            "https://telegra.ph/file/e348b424c102b7cf5149c.jpg",
+            "https://telegra.ph/file/1c3ae2eaecc9cec2f0845.jpg",
+            "https://telegra.ph/file/781ee29145a92ef1ee268.jpg",
+            "https://telegra.ph/file/1d6eefc7abc22b81eb26e.jpg",
+            "https://telegra.ph/file/eed05ef59c9fb2e84d0df.jpg",
+            "https://telegra.ph/file/87c7e8b40622f8722e668.jpg",
+            "https://telegra.ph/file/3c7d419b0de3486c926c6.jpg",
+            "https://telegra.ph/file/29eecc5832c33d2c0b327.jpg",
+            "https://telegra.ph/file/36dfbcecc189fed26db72.jpg",
+            "https://telegra.ph/file/a8c889c5fe2753dfc20a8.jpg",
+            "https://telegra.ph/file/e94d870996634050ac9ec.jpg",
+            "https://telegra.ph/file/3099888c66800f387af4b.jpg",
+            "https://telegra.ph/file/441d58f6e1fadc3a5b9e7.jpg",
+            "https://telegra.ph/file/8192782cabbbae6a71347.jpg",
+            "https://telegra.ph/file/258712b87357a51d4fc4c.jpg",
+            "https://telegra.ph/file/66887d27f07fa410b140c.jpg",
+            "https://telegra.ph/file/3e6fb6f3648a93217ec9a.jpg"
+       )
