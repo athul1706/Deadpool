f01eb5c50c5e59ace3685df87910ec185cf29fdd
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2022-12-18 03:45:28 +0530

    Trying to fix pm spell check

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -444,7 +444,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             protect_content=True if ident == 'checksubp' else False
         )
     elif query.data == "pages":
-        await query.answer("⚠️ Clicking page buttons will not work!")
+        await query.answer("⚠️ Clicking page buttons will not work!", show_alert=True)
     elif query.data == "start":
         now = datetime.datetime.now()
         tz = pytz.timezone('Asia/Kolkata')
@@ -1100,3 +1100,26 @@ async def nextp_page(bot, query):
     except MessageNotModified:
         pass
     await query.answer()
+
+@Client.on_callback_query(filters.regex(r"^spollingpm"))
+async def advantage_spoll_choker(bot, query):
+    _, user, movie_ = query.data.split('#')
+    if int(user) != 0 and query.from_user.id != int(user):
+        return await query.answer("okDa", show_alert=True)
+    if movie_ == "close_spellcheck":
+        return await query.message.delete()
+    movies = SPELL_CHECK.get(query.message.id)
+    if not movies:
+        return await query.answer("You are clicking on an old button which is expired.", show_alert=True)
+    movie = movies[(int(movie_))]
+    await query.answer('Checking for Movie in database...')
+    k = await manual_filters(bot, query.message, text=movie)
+    if k == False:
+        files, offset, total_results = await get_search_results(movie, offset=0, filter=True)
+        if files:
+            k = (movie, files, offset, total_results)
+            await pmauto_filter(bot, query, k)
+        else:
+            k = await query.message.edit('This Movie Not Found In DataBase')
+            await asyncio.sleep(10)
+            await k.delete()
