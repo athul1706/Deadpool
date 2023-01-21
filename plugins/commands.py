37aba8fd67e611e7209294f4fb729e627ecdd9c1
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2023-01-21 23:06:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -571,7 +571,7 @@ async def request_movie_files(bot, query):
     if athul == "unupload":
         await query.message.delete() 
         await bot.send_message(int(from_user), 
-                               "Sorry..Your request for movie is declined by admin, because it is already in the bot. Check with correct spelling", 
+                               "Sorry..Your request for movie {query.message.text} is declined by admin, because it is already in the bot. Check with correct spelling🙂", 
                                reply_to_message_id=int(lst_msg_id))
         return
     if athul == "notfound":
@@ -582,6 +582,7 @@ async def request_movie_files(bot, query):
         return
         
     if int(from_user) not in ADMINS:
+        await query.message.delete()
         await bot.send_message(int(from_user), 
                                "Your requested file is successfully uploaded. Thank you for using this feature. Stay in touch with us🥰", 
                                reply_to_message_id=int(lst_msg_id))
