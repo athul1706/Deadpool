fec105ae27aa9fefbd233881303fa4a46ae78395
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2023-06-26 00:27:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -913,6 +913,69 @@ async def manual_filters(client, message, text=False):
 
 
 
+
+
+@Client.on_callback_query(filters.regex(r"^ynext"))
+async def next_page(bot, query):
+
+    ident, req, key, offset = query.data.split("_")
+    if int(req) not in [query.from_user.id, 0]:
+        return await query.answer("oKda", show_alert=True)
+    try:
+        offset = int(offset)
+    except:
+        offset = 0
+    search = BUTTONS.get(key)
+    if not search:
+        await query.answer("You are using this for one of my old message, please send the request again.",show_alert=True)
+        return
+
+    files, n_offset, total = await get_search_results(search, offset=offset, filter=True)
+    try:
+        n_offset = int(n_offset)
+    except:
+        n_offset = 0
+
+    if not files:
+        return
+    btn = [
+        [
+            InlineKeyboardButton(
+                text=f"📂[{get_size(file.file_size)}]📂 {file.file_name}", callback_data=f'mypmfile#{file.file_id}'
+            ),
+            
+        ]
+        for file in files
+    ]
+
+    if 0 < offset <= 10:
+        off_set = 0
+    elif offset == 0:
+        off_set = None
+    else:
+        off_set = offset - 10
+    if n_offset == 0:
+        btn.append(
+            [InlineKeyboardButton("⏪ BACK", callback_data=f"ynext_{req}_{key}_{off_set}"), InlineKeyboardButton(f"📃 Pages {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")]
+        )
+    elif off_set is None:
+        btn.append([InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("NEXT ⏩", callback_data=f"ynext_{req}_{key}_{n_offset}")])
+    else:
+        btn.append(
+            [
+                InlineKeyboardButton("⏪ BACK", callback_data=f"ynext_{req}_{key}_{off_set}"),
+                InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"),
+                InlineKeyboardButton("NEXT ⏩", callback_data=f"ynext_{req}_{key}_{n_offset}")
+            ],
+        )
+    try:
+        await query.edit_message_reply_markup( 
+            reply_markup=InlineKeyboardMarkup(btn)
+        )
+    except MessageNotModified:
+        pass
+    await query.answer()
+
 @Client.on_message(filters.text & filters.private)
 async def privat_in(client, message):
     
@@ -927,7 +990,7 @@ async def privat_in(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                text=f"📂[{get_size(file.file_size)}]📂 {file.file_name}", url=f'https://t.me/TGMovieRobot?start=_-_-_-_{file.file_id}'
+                text=f"📂[{get_size(file.file_size)}]📂 {file.file_name}", callback_data=f'mypmfile#{file.file_id}'
             ),
             ]
             for file in files
@@ -939,7 +1002,7 @@ async def privat_in(client, message):
             req = message.from_user.id if message.from_user else 0
             mention = message.from_user.mention
             btn.append(
-                [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_{req}_{key}_{offset}")]
+                [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="NEXT ⏩",callback_data=f"ynext_{req}_{key}_{offset}")]
             )
         else:
             btn.append(
