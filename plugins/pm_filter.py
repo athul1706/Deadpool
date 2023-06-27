67dd2f6dadfc7f057a01b2d6fa618f5394e6c2bf
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2023-06-27 13:10:51 +0530

    Update pm_filter.py
    small fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -77,7 +77,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'mypmfile#{file.file_id} if chat_type == enums.ChatType.PRIVATE else files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=(f'mypmfile#{file.file_id} if chat_type == enums.ChatType.PRIVATE else files#{file.file_id}')
                 ),
             ]
             for file in files
